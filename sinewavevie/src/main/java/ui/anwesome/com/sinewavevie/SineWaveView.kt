6a6a44e03d67119f3adb497ee2457925786b3399
Anwesh43 <anweshthecool0@gmail.com>
2018-01-13 01:06:19 +0530

    adding SineWave

--- sinewavevie/src/main/java/ui/anwesome/com/sinewavevie/SineWaveView.kt
+++ sinewavevie/src/main/java/ui/anwesome/com/sinewavevie/SineWaveView.kt
@@ -6,6 +6,8 @@ package ui.anwesome.com.sinewavevie
 import android.view.*
 import android.content.*
 import android.graphics.*
+import java.util.concurrent.ConcurrentLinkedQueue
+
 class SineWaveView(ctx:Context):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
@@ -14,4 +16,43 @@ class SineWaveView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         return true
     }
+    data class SineWave(var x:Float,var y:Float,var a_y:Float,var a_x:Float) {
+        var points:ConcurrentLinkedQueue<PointF> = ConcurrentLinkedQueue()
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            val path = Path()
+            var i = 0
+            points.forEach {
+                if(i == 0) {
+                    path.moveTo(it.x,it.y)
+                }
+                else {
+                    path.lineTo(it.x,it.y)
+                }
+                i++
+            }
+            canvas.drawPath(path,paint)
+            canvas.restore()
+        }
+        fun updateXY(x:Float,y:Float) {
+            points.add(PointF(x,y))
+        }
+        fun removePoints(stopcb:()->Unit) {
+            if(points.size > 0) {
+                points.removeFirst()
+                stopcb()
+            }
+        }
+    }
+}
+fun ConcurrentLinkedQueue<PointF>.removeFirst() {
+    var i = 0
+    forEach {
+        if(i == 0) {
+            remove(it)
+        }
+        i++
+    }
+
 }
