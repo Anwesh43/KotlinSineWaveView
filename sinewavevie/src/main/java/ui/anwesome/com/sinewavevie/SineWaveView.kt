29a584056d91e5076a25d6c4f6c6a87065913cf7
Anwesh43 <anweshthecool0@gmail.com>
2018-01-13 00:46:00 +0530

    adding SineWaveView class

--- sinewavevie/src/main/java/ui/anwesome/com/sinewavevie/SineWaveView.kt
+++ sinewavevie/src/main/java/ui/anwesome/com/sinewavevie/SineWaveView.kt
@@ -3,3 +3,15 @@ package ui.anwesome.com.sinewavevie
 /**
  * Created by anweshmishra on 13/01/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class SineWaveView(ctx:Context):View(ctx) {
+    val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        return true
+    }
+}
