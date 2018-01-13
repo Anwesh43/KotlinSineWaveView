776831f468affdbc5bd2449622be5a891f1d07af
Anwesh43 <anweshthecool0@gmail.com>
2018-01-13 16:04:37 +0530

    adding KotlinSineWaveView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinsinewaveview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinsinewaveview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinsinewaveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.sinewavevie.SineWaveView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SineWaveView.create(this)
     }
 }
