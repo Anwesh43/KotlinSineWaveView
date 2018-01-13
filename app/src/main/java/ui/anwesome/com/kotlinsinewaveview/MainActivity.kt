59859e18a768c123cdd10df9dde512a9057746a8
Anwesh43 <anweshthecool0@gmail.com>
2018-01-13 18:14:48 +0530

    adding OnSineWaveListener

--- app/src/main/java/ui/anwesome/com/kotlinsinewaveview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinsinewaveview/MainActivity.kt
@@ -2,12 +2,16 @@ package ui.anwesome.com.kotlinsinewaveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.sinewavevie.SineWaveView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SineWaveView.create(this)
+        val view = SineWaveView.create(this)
+        view.addOnSineWaveListener { ox,dx ->
+            Toast.makeText(this,"moved from $ox to $dx",Toast.LENGTH_SHORT).show()
+        }
     }
 }
